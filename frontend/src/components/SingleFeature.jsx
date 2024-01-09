917f934fc3a4a5b62cff0d0c02f0e82b1e43aa3c
Arnav Phad <adphad123@gmail.com>
2024-01-09 23:17:42 +0530

    About us

--- frontend/src/components/SingleFeature.jsx
+++ frontend/src/components/SingleFeature.jsx
@@ -9,12 +9,12 @@ import resume from '../assets/resume.png'
 
 export function SingleFeature() {
     return (
-        <Card className={`rounded-3xl px-6 m-6 flex-col items-center justify-center w-[300px] max-w-[26rem] h-[500px] shadow-lg bg-gradient-radial from-lightBlack via-darkBlack to-black`} >
-            <CardHeader floated={false} className="shadow-md border-[1E262A] border-spacing-0 border-8 rounded-full h-[150px] w-[150px] mb-8 ">
+        <Card className={`rounded-3xl px-6 m-6 flex-col items-center justify-center lg:max-w-[26rem] h-100 w-60 shadow-lg bg-gradient-radial from-lightBlack via-darkBlack to-black`} >
+            <CardHeader floated={false} className="shadow-md border-[1E262A] border-spacing-0 border-8 rounded-full lg:h-[150px] lg:w-[150px] mb-8 h-28 w-28">
             <img
                 src={resume}
                 alt="img"
-                className="h-[150px] w-[150px] object-scale-down"
+                className="h-28 w-28 object-scale-down"
             />
             </CardHeader>
             <CardBody>
@@ -25,13 +25,13 @@ export function SingleFeature() {
             
             </div>
             <hr />
-            <Typography color="white">
+            <Typography color="white" className="text-sm font-light">
                 Enter a freshly updated and thoughtfully furnished peaceful home
                 surrounded by ancient trees, stone walls, and open meadows.
             </Typography>
             
             </CardBody>
-            <CardFooter className="pb-0 flex justify-center items-center">
+            <CardFooter className="pb-4 flex justify-center items-center">
             <button className="bgGradient px-4 py-2 mt-4 text-sm rounded-3xl font-semibold text-black">
                 TRY IT NOW
             </button>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,28 +1,48 @@
 import img1 from "../assets/img1.png"
 import arrow from "../assets/arrow.png"
 import SingleFeature from "../components/SingleFeature";
+import About from "../components/About";
+
 const Home = () => {
     return (
         <div className="text-white max-w-[1300px] px-10 mx-auto w-screen">
             <div className="flex gap-4">
                 <div className="mt-16 lg:w-[67%] lg:ml-10 lg:block flex flex-col items-center ">
-                    <h1 className="text-xl min-[440px]:text-2xl min-[540px]:text-3xl sm:text-4xl font-medium">Supercharge Your Career with</h1>
-                    <h1 className="textGradient mt-2 lg:mt-4 lg:ml-44 text-xl min-[440px]:text-2xl min-[540px]:text-3xl sm:text-4xl font-semibold">CVsavvy</h1>
-                    <p className="mt-4 sm:mt-8 lg:max-w-[600px] text-xs sm:text-sm md:w-full">Our free <span className="font-semibold">AI-powered</span> resume checker scores your resume on key criteria recruiters and hiring managers look for. Get actionable steps to revamp your resume and <span className="font-semibold">land more interviews.</span></p>
-                    <button className="bgGradient py-2 px-6 mt-6 lg:mt-8 lg:ml-40 text-sm rounded-3xl font-medium text-black">Check your Resume</button>
+                    <h1 className="text-xl min-[440px]:text-2xl min-[540px]:text-3xl sm:text-4xl font-medium ml-2 lg:ml-0">Supercharge Your Career with 
+                        <span className="textGradient mt-2 pr-8 text-xl min-[440px]:text-2xl min-[540px]:text-3xl sm:text-4xl font-semibold "> CVsavvy</span>
+                    </h1>
+                
+                    <p className="mt-4 sm:mt-8 lg:max-w-[600px] text-xs sm:text-sm md:w-full ml-2 lg:ml-0">Our free <span className="font-semibold">AI-powered</span> resume checker scores your resume on key criteria recruiters and hiring managers look for. Get actionable steps to revamp your resume and <span className="font-semibold">land more interviews.</span></p>
+                    <button className="bgGradient py-2 px-6 mt-6 lg:mt-8 lg:ml-40 text-sm rounded-3xl font-semibold text-black">Check your Resume</button>
                 </div>
                 <div className="mt-7 mr-8 hidden lg:block">
                     <img src={img1} alt="hello" className="h-[350px] w-[400px]" />
                 </div>
             </div>
-            <div className="flex ml-10">
-                <div>WHY USE CVSAVVY</div>
-                <img src={arrow} alt="" />
-                <div>From simple spelling mistakes to more hard-to-notice issues like passive language, there's a lot that could be wrong with your resume. The average job seeker might not notice these issues - but employers do. Our resume checker was designed by NALLE LOG and VELLE LOG to scan for all the details that hiring managers look for when reviewing a job applicant. Then it gives you immediate feedback on ways you can improve your resume.</div>
+            <div className="flex lg:ml-10 lg:my-8 mb-6 lg:flex-row mt-6 flex-col items-start ml-2">
+                <div className="lg:mr-2 text-2xl font-extrabold mb-4 flex flex-row items-center lg:flex lg:flex-row">WHY USE CVSAVVY
+                    <img src={arrow} alt="" className="px-4 lg:object-scale-down h-20 w-48 m-0 hidden md:block lg:block"/>  
+                    <div className="lg:text-sm text-xs font-light lg:flex lg:flex-col hidden">From simple spelling mistakes to more hard-to-notice issues like passive language, there's a lot that could be wrong with your resume. <br /> The average job seeker might not notice these issues - <span className="font-bold">but employers do.</span> <br /> Our resume checker was designed by NALLE LOG and VELLE LOG to scan for all the details that hiring managers look for when reviewing a job applicant. Then it gives you immediate feedback on ways you can improve your resume.</div>
+                </div>
+                <div className="text-xs font-light lg:hidden">From simple spelling mistakes to more hard-to-notice issues like passive language, there's a lot that could be wrong with your resume. <br /> The average job seeker might not notice these issues - <span className="font-bold">but employers do.</span> <br /> Our resume checker was designed by NALLE LOG and VELLE LOG to scan for all the details that hiring managers look for when reviewing a job applicant. Then it gives you immediate feedback on ways you can improve your resume.</div>
             </div>
-            <SingleFeature />
+            <div className="flex flex-wrap justify-evenly lg:mx-8">
+                <SingleFeature />
+                <SingleFeature />
+                <SingleFeature />
+                <SingleFeature />
+            </div>
+            <div className="flex lg:ml-10 lg:my-8 mb-6 lg:flex-row mt-6 flex-col items-start">
+                <div className="lg:mr-2 text-2xl font-extrabold mb-4 flex flex-row items-center lg:flex lg:flex-row">INTRODUCTION 
+                    <span className="textGradient pl-2"> CVsavvy</span>
+                    <img src={arrow} alt="" className="px-4 lg:object-scale-down h-20 w-48 m-0 hidden md:block lg:block"/>  
+                    <div className="lg:text-sm text-xs font-light lg:flex lg:flex-col hidden">From simple spelling mistakes to more hard-to-notice issues like passive language, there's a lot that could be wrong with your resume. <br /> The average job seeker might not notice these issues - <span className="font-bold">but employers do.</span> <br /> Our resume checker was designed by NALLE LOG and VELLE LOG to scan for all the details that hiring managers look for when reviewing a job applicant. Then it gives you immediate feedback on ways you can improve your resume.</div>
+                </div>
+                <div className="text-xs font-light lg:hidden">From simple spelling mistakes to more hard-to-notice issues like passive language, there's a lot that could be wrong with your resume. <br /> The average job seeker might not notice these issues - <span className="font-bold">but employers do.</span> <br /> Our resume checker was designed by NALLE LOG and VELLE LOG to scan for all the details that hiring managers look for when reviewing a job applicant. Then it gives you immediate feedback on ways you can improve your resume.</div>
+            </div>
+            <About />
         </div>
     );
 }
- 
+
 export default Home;
